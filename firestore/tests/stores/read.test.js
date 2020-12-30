5cb76bd59ddd4551445774c52fd8f44dd28b2fe8
Dan Purdy <danjpurdy@gmail.com>
2020-12-30 22:11:48 +0000

    Add store read rules

---
+++ firestore/tests/stores/read.test.js
@@ -0,0 +1,79 @@
+const { setup, teardown } = require('../helpers');
+const { assertSucceeds } = require('@firebase/rules-unit-testing');
+
+describe('Stores read rules', () => {
+  describe('list', () => {
+    afterAll(async () => {
+      await teardown();
+    });
+
+    test('succeed when a non authenticated user tries to get a list of all stores', async () => {
+      const db = await setup(
+        null,
+        {
+          'stores/ST01': {
+            name: 'test1',
+          },
+        },
+        {
+          'stores/STO2': {
+            name: 'test2',
+          },
+        },
+      );
+      const ref = db.collection('stores');
+
+      expect(await assertSucceeds(ref.get()));
+    });
+
+    test('succeed when a authenticated user tries to load a list of stores', async () => {
+      const db = await setup(
+        {
+          uid: 'user',
+        },
+        {
+          'stores/ST01': {
+            name: 'test1',
+          },
+        },
+        {
+          'stores/STO2': {
+            name: 'test2',
+          },
+        },
+      );
+      const ref = db.collection('stores');
+
+      expect(await assertSucceeds(ref.doc('ST01').get()));
+    });
+  });
+
+  describe('get', () => {
+    test('succeed when a non authenticated user tries to load a shop', async () => {
+      const db = await setup(null, {
+        'stores/ST01': {
+          name: 'test',
+        },
+      });
+      const ref = db.collection('stores');
+
+      expect(await assertSucceeds(ref.doc('ST01').get()));
+    });
+
+    test('succeed when a authenticated user tries to load a shop', async () => {
+      const db = await setup(
+        {
+          uid: 'user',
+        },
+        {
+          'stores/ST01': {
+            name: 'test',
+          },
+        },
+      );
+      const ref = db.collection('stores');
+
+      expect(await assertSucceeds(ref.doc('ST01').get()));
+    });
+  });
+});
